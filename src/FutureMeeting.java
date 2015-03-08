6bb2bb1c76c674d153f2295be2a540bbd3f21c1b
Annabel Jump <annabel.jump@gmail.com>
2015-03-08 18:39:52 +0000

    First commit, interfaces provided in coursework specification added

---
+++ src/FutureMeeting.java
@@ -0,0 +1,13 @@
+/**
+ * Default interface provided by Programming in Java tutors
+ * for Coursework number 4 - not edited from coursework PDF file
+ * NOT written by Annabel Jump, WRITTEN BY PiJ Tutors!
+ */
+
+/**
+ * A meeting to be held in the future
+ */
+public interface FutureMeeting extends Meeting{
+// No methods here, this is just a naming interface
+// (i.e. only necessary for type checking and/or downcasting)
+}
