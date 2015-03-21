6707026ba48ad271906c289bafd90b21c23ad05a
Annabel Jump <annabel.jump@gmail.com>
2015-03-21 22:33:07 +0000

    Contact constructors added

--- ContactImpl.java
+++ ContactImpl.java
@@ -12,4 +12,30 @@ public class ContactImpl implements Contact {
     private String name;
     private String notes;
 
+    //Constructors
+
+    ContactImpl(String thisName){
+        this.name = thisName;
+        this.identification = null;
+        this.notes = "";
+    }
+
+    ContactImpl(int iD){
+        this.identification = iD;
+        this.name = "Unknown";
+        this.notes = "";
+    }
+
+    ContactImpl(String contactName, int number){
+        this.identification = number;
+        this.name = contactName;
+        this.notes = "";
+    }
+
+    ContactImpl(String aName, int iDent, String comments){
+        this.identification = iDent;
+        this.name = aName;
+        this.notes = comments;
+    }
+
 }
