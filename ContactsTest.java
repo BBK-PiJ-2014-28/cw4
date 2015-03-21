82f4d5a0f8ebd534900cb069ac2817d55a2b1e2a
Annabel Jump <annabel.jump@gmail.com>
2015-03-21 14:46:20 +0000

    Tests for interface added to ContactsTest - failure expected as no code yet written

--- ContactsTest.java
+++ ContactsTest.java
@@ -1,35 +1,53 @@
-package testing;
-
 /**
- * File to Test Contacts/ContactsImpl
+ * File to Test Contacts
  * @author Annabel Jump
  * for PiJ Coursework 4
  */
 
 
-import src.Contacts;
-import src.ContactsIpl;
 import java.io.*;
-
 import org.junit.*;
 import org.junit.Assert.*;
 
-public class ContactsImplTest {
+public class ContactsTest {
 
     public Contact testContact;
 
-    //initialise (nonexistant) new Contact for test
-    @before
+    //initialise variables for new Contact for test
+    //integer for ID and String for name
+    @Before
     public void buildUp() {
-        testContact = new Contact();
+        testContact = new Contact(1, "Rowan");
     }
 
-    //reset after (hopefully un-) successful test
-    @after
+    //reset after test
+    @After
     public void cleanUp() {
         testContact = null;
     }
 
+    //test getId method
+    @Test
+    public void testIDfetch() {
+        assertEquals(1, testContact.getId());
+    }
 
+    //test getName method
+    @Test
+    public void testNameFetch() {
+        assertEquals("Rowan", testContact.getName());
+    }
 
+    //test getNotes with empty notes
+    @Test
+    public void testGetEmptyNotes() {
+        assertEquals("", testContact.getNotes());
+    }
+
+    //test addNotes by adding notes and fetching them
+    @Test
+    public void testAddFakeNotes() {
+        testContact.addNotes("Rowan is Callisto Prime");
+        assertEquals("Rowan is Callisto Prime", testContact.getNotes());
+    }
 }
