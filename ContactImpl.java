e38569f0f03638ef1935e556036f4278de5d3ce8
Annabel Jump <annabel.jump@gmail.com>
2015-03-21 15:20:08 +0000

    Started implementation of Contact Interface

---
+++ ContactImpl.java
@@ -0,0 +1,15 @@
+/**
+ * File to Implement Contact interface
+ * @author Annabel Jump
+ * for PiJ Coursework 4
+ */
+
+import java.io.*;
+
+public class ContactImpl implements Contact {
+
+    private int identification;
+    private String name;
+    private String notes;
+
+}
