599256e6f3598e54fa2041c71151a8babf2a0c83
Annabel Jump <annabel.jump@gmail.com>
2015-03-22 17:19:34 +0000

    MeetingImpl and MeetingTest initialised

---
+++ MeetingImpl.java
@@ -0,0 +1,31 @@
+/**
+ * File to Implement Meeting interface
+ * @author Annabel Jump
+ * for PiJ Coursework 4
+ */
+
+import java.util.Calendar;
+import java.util.Set;
+
+
+public class MeetingImpl implements Meeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
