9dfae26d2b54f5eb19d33dc8eddf0a4a2e66779d
Annabel Jump <annabel.jump@gmail.com>
2015-03-22 17:50:38 +0000

    Some Tests added to MeetingTest - not yet completed all tests

--- MeetingTest.java
+++ MeetingTest.java
@@ -1,12 +1,52 @@
+/**
+ * File to Test Meeting interface (and implementation)
+ * @author Annabel Jump
+ * for PiJ Coursework 4
+ */
+
+
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Set;
+
 
 
 public class MeetingTest {
 
+	public Meeting testMeeting;
+	public Calendar meetingDate;
+	public Set<Contact> testSet;
+	public Contact rowan, raven;
+	public int iD = 12;
+	
+	@Before
+	public void buildUp(){
+		rowan = new ContactImpl("Rowan Gwyn");
+		raven = new ContactImpl("Jeff Raven");
+		testSet.add(rowan);
+		testSet.add(raven);
+		meetingDate = new GregorianCalendar(2015, 8, 28, 6, 33);
+		testMeeting = new Meeting(meetingDate, testSet, iD);
+	}
+	
+	@After
+	public void cleanUp(){
+		testMeeting = null;
+	}
+	
 	@Test
-	public void test() {
+	public void testGetMeetingID() {
+		assertEquals(12, testMeeting.getId());
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetDate() {
+		Calendar correctDate = new GregorianCalendar(2015, 8, 28, 6, 33);
+		assertEquals(correctDate, testMeeting.getDate());
 		fail("Not yet implemented");
 	}
 
