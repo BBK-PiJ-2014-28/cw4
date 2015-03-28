b226db028a227595b2f93b4f3d08eacb1482edb9
Annabel Jump <annabel.jump@gmail.com>
2015-03-28 19:21:50 +0000

    Added all constructors

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -15,6 +15,52 @@ public class MeetingImpl implements Meeting {
 	public Calendar requestedMeeting;
 	public Set<Contact> meetersAndGreeters;
 
+	//Constructors
+	
+	MeetingImpl(int iDent){
+		this.iD = iDent;
+		this.requestedMeeting = null; //should throw exception
+		this.meetersAndGreeters = null; //should throw exception
+	}
+	
+	MeetingImpl(Calendar reserveTheBoardroom){
+		this.iD = 0; //will need to make sure 0 throws exception
+		this.requestedMeeting = reserveTheBoardroom;
+		this.meetersAndGreeters = null; //exception to be thrown
+	}
+	
+	MeetingImpl(Set<Contact> theApprentice){
+		this.iD = 0; //exception to be thrown
+		this.requestedMeeting = null; //exception
+		this.meetersAndGreeters = theApprentice;
+	}
+	
+	//the only fully acceptable way to initialise a meeting - all variables accounted for	
+	MeetingImpl(int iDent, Calendar reserveTheBoardroom, Set<Contact> theApprentice){
+		this.iD = iDent;
+		this.requestedMeeting = reserveTheBoardroom;
+		this.meetersAndGreeters = theApprentice;
+	}
+	
+	MeetingImpl(int iDent, Calendar reserveTheBoardroom){
+		this.iD = iDent;
+		this.requestedMeeting = reserveTheBoardroom;
+		this.meetersAndGreeters = null; //this should throw exception
+	}
+	
+	MeetingImpl(int iDent, Set<Contact> theApprentice){
+		this.iD = iDent;
+		this.requestedMeeting = null; //exception
+		this.meetersAndGreeters = theApprentice;
+	}
+	
+	MeetingImpl(Calendar reserveTheBoardroom, Set<Contact> theApprentice){
+		this.iD = 0; //exception
+		this.requestedMeeting = reserveTheBoardroom;
+		this.meetersAndGreeters = theApprentice;
+	}
+	
+	
 	@Override
 	public int getId() {
 		// TODO Auto-generated method stub
