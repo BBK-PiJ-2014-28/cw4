edb2a9dec8608da73f1de6371894316ec1c7eb74
Annabel Jump <annabel.jump@gmail.com>
2015-03-28 19:01:05 +0000

    Started on MeetingImpl

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -5,10 +5,15 @@
  */
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Set;
 
 
 public class MeetingImpl implements Meeting {
+	
+	public int iD;
+	public Calendar requestedMeeting;
+	public Set<Contact> meetersAndGreeters;
 
 	@Override
 	public int getId() {
