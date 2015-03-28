d9596ff4614f4b4df10e8b8e67f014417f4ff0be
Annabel Jump <annabel.jump@gmail.com>
2015-03-28 22:28:21 +0000

    Tests for null constructions added

--- MeetingTest.java
+++ MeetingTest.java
@@ -38,11 +38,6 @@ public class MeetingTest {
 	}
 	
 	@Test
-	public void testSetUp(){
-		//TODO insert tests for different constructions
-	}
-	
-	@Test
 	public void testGetMeetingID() {
 		testMeeting = new MeetingImpl(12, meetingDate, testSet);
 		assertEquals(12, testMeeting.getId());
@@ -51,6 +46,7 @@ public class MeetingTest {
 	
 	@Test
 	public void testGetDate() {
+		testMeeting = new MeetingImpl(12, meetingDate, testSet);
 		Calendar correctDate = new GregorianCalendar(2015, 8, 28, 6, 33);
 		assertEquals(correctDate, testMeeting.getDate());
 		fail("Not yet implemented");
@@ -58,9 +54,44 @@ public class MeetingTest {
 	
 	@Test
 	public void testGetContacts() {
+		testMeeting = new MeetingImpl(12, meetingDate, testSet);
 		correctSet.add(rowan);
 		correctSet.add(raven);
 		assertEquals(correctSet, testMeeting.getContacts());
 		fail("Not yet implemented");	
 	}
+	
+	//Test of constructors at end so that other (necessary) methods tested first
+	
+	@Test
+	public void testSetUp1(){
+		testMeeting = new MeetingImpl(12, meetingDate);
+		assertEquals(null, testMeeting.getContacts());
+	}
+	
+	@Test
+	public void testSetUp2(){
+		testMeeting = new MeetingImpl(meetingDate, testSet);
+		assertEquals(0, testMeeting.getId());
+	}
+	
+	@Test
+	public void testSetUp3(){
+		testMeeting = new MeetingImpl(12, testSet);
+		assertEquals(null, testMeeting.getDate());
+	}
+	
+	@Test
+	public void testSetUp4(){
+		testMeeting = new MeetingImpl(12);
+		Meeting secondTest = new MeetingImpl(meetingDate);
+		Meeting thirdTest = new MeetingImpl(testSet);
+		assertEquals(null, testMeeting.getDate());
+		assertEquals(null, testMeeting.getContacts());
+		assertEquals(null, secondTest.getContacts());
+		assertEquals(0, secondTest.getId());
+		assertEquals(null, thirdTest.getDate());
+		assertEquals(0, thirdTest.getId());
+	}
+	
 }
