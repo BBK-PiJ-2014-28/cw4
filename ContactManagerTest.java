831a7035e9267120f69ccad31fcfc730e13468ec
Annabel Jump <annabel.jump@gmail.com>
2015-03-29 18:16:51 +0100

    Before added, initialising variables. After resets variables and calls flush() - will need to add deletion of file, perhaps AfterClass

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -7,10 +7,12 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.List;
 
 import java.io.*;
 
@@ -19,9 +21,43 @@ public class ContactManagerTest {
 	public int meetingCounter, contactCounter;
 	public File testData;
 	public ContactManager testManager;
-	public Calendar testCalendar;
+	public Calendar testCalendar, todayTodayToday;
 	public Set<Contact> testContacts;
 	public Contact rowan, raven;
+	
+	//Initialise variable before tests
+	
+	@Before
+	public void buildUp(){
+		rowan = new ContactImpl("Rowan Gwyn");
+		raven = new ContactImpl("Jeff Raven");
+		rowan.addNotes("Callisto T1 Prime");
+		raven.addNotes("Deneb Untrained T1 Prime");
+		testContacts = new HashSet<Contact>();
+		testContacts.add(rowan);
+		testContacts.add(raven);
+		testData = new File("testContacts.txt");
+		meetingCounter = 0;
+		contactCounter = 0;
+		todayTodayToday = GregorianCalendar.getInstance();
+		testCalendar = new GregorianCalendar(2015, 8, 28, 6, 33);
+		testManager = new ContactManagerImpl();		
+	}
+	
+	//Close down all variables after tests
+	
+	@After
+	public void cleanUp(){
+		testManager.flush();
+		rowan = null;
+		raven = null;
+		testContacts = null;
+		meetingCounter = 0;
+		contactCounter = 0;
+		todayTodayToday = null;
+		testCalendar = null;
+		testManager = null;
+	}
 	
 	@Test
 	public void testAddFutureMeeting() {
