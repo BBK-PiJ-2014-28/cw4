5d0da6292dbdf5966f5abf17b8bf1e4037fa68d2
Annabel Jump <annabel.jump@gmail.com>
2015-03-29 20:46:14 +0100

    Finalised FutureMeetingImpl with Serialization

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -3,8 +3,11 @@ import java.util.Set;
 import java.io.*;
 
 
-public class FutureMeetingImpl implements FutureMeeting, Serializable {
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting, Serializable {
 
+	public FutureMeetingImpl(int iD, Calendar reserveTheBoardroom, Set<Contact> theApprentice){
+		super(iD, reserveTheBoardroom, theApprentice);
+	}
 	
 
 }
