4003be996bf582fd860efb92e0d06556b58bde98
Annabel Jump <annabel.jump@gmail.com>
2015-03-29 17:53:27 +0100

    Variables inside test added

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -1,3 +1,9 @@
+/**
+ * Class to Test ContactManager using JUnit
+ * @author Annabel Jump
+ * for PiJ Coursework 4
+ */
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -10,9 +16,16 @@ import java.io.*;
 
 public class ContactManagerTest {
 
+	public int meetingCounter, contactCounter;
+	public File testData;
+	public ContactManager testManager;
+	public Calendar testCalendar;
+	public Set<Contact> testContacts;
+	public Contact rowan, raven;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddFutureMeeting() {
+		
 	}
 
 }
