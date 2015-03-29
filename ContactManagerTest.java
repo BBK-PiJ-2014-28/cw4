0e2ee6338e06ac78f4818ea48377d89f91d2d2d5
Annabel Jump <annabel.jump@gmail.com>
2015-03-29 17:04:22 +0100

    Initialised ContactManagerImpl and Test

---
+++ ContactManagerTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Set;
+import java.util.HashSet;
+
+import java.io.*;
+
+public class ContactManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
